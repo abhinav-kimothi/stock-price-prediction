bece542a9c88946e90d7d79de83e9c6abd4aec9e
abhinavkimothi <abhinav.kimothi.ds@gmail.com>
2023-01-14 00:05:03 +0530

    modified app.js

--- sppred/src/App.js
+++ sppred/src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [selectedStock, setSelectedStock] = useState('');
 
-export default App;
+  const handleChange = (event) => {
+      setSelectedStock(event.target.value);
+  }
+
+  return (      
+      <div>
+          <label>Select a stock:</label>
+          <select value={selectedStock} onChange={handleChange}>
+              <option value="AAPL">Apple</option>
+              <option value="MSFT">Microsoft Corp.</option>
+              <option value="AMZN">Amazon.com Inc.</option>
+              <option value="TSLA">Tesla Inc.</option>
+              <option value="GOOGL">Alphabet Inc.</option>
+          </select>
+      </div>
+  );
+  }
+
+  export default App;
--- sppred/src/index.js
+++ sppred/src/index.js
@@ -5,35 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-function StockSelector() {
-    const [selectedStock, setSelectedStock] = useState('');
 
-    const handleChange = (event) => {
-        setSelectedStock(event.target.value);
-    }
 
-    return (      
-        <div>
-            <label>Select a stock:</label>
-            <select value={selectedStock} onChange={handleChange}>
-                <option value="AAPL">Apple</option>
-                <option value="MSFT">Microsoft Corp.</option>
-                <option value="AMZN">Amazon.com Inc.</option>
-                <option value="TSLA">Tesla Inc.</option>
-                <option value="GOOGL">Alphabet Inc.</option>
-            </select>
-        </div>
-    );
-}
 
-export default StockSelector;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App>
-      <StockSelector />
-      </App>
+    
+      <App />
+    
   </React.StrictMode>
 );
 
